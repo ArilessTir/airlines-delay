b76c791c9183fafa4a3a29c387322e9d6c2064d0
ArilessTir <ariless.tir@gmail.com>
2022-07-26 11:48:26 +0200

    update transformer and mlflow"

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,10 +2,25 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from preprocessing import preprocessor
 
+model = {
+    'name':LogisticRegression,
+    'model_params':{    
+        'max_iter':200,
+        'random_state':400,
+        'C':10
+    }
+}
 
-model = Pipeline(
+pipeline = Pipeline(
     steps=[
         ('preprocessing', preprocessor),
-        ('model',LogisticRegression(max_iter=500,random_state=400))
+        ('model',model['name'](**model['model_params']))
     ]
 )
+
+prepross_list = pipeline.named_steps['preprocessing'].transformers
+prepross = {}
+
+for i in range(len(prepross_list)):
+    prepross[prepross_list[i][0]] = prepross_list[i]
+
